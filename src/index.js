8533a3fb4dec0860aa5986916cac37ee450a7095
Juveria Manzar <juveriamanzar29@gmail.com>
2020-10-17 00:05:46 +0530

    initial-commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const BookList = ()=>{
+  return (
+    <section>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+    </section>
+  );
+}
+
+const Book = () => (
+  <article>
+    <Image/>
+    <Title/>
+    <Author/>
+  </article>
+)
+
+const Image = () => (
+  <img
+    src="https://images-eu.ssl-images-amazon.com/images/I/71t4GuxLCuL._AC_UL200_SR200,200_.jpg"
+    alt=""
+  />
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Title = () => <h1>The Subtle Art of Not Giving a F*ck</h1>;
+
+const Author = () => <h4>Mark Manson</h4>;
+
+
+ReactDom.render(<BookList/>, document.getElementById('root'))
