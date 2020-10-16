b9e98a00ff32c84efd587af00936d4abbbff81b3
Juveria Manzar <juveriamanzar29@gmail.com>
2020-10-17 01:37:07 +0530

    JSX-css

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const BookList = ()=>{
 }
 
 const Book = () => (
-  <article>
+  <article className="book">
     <Image/>
     <Title/>
     <Author/>
@@ -33,7 +33,7 @@ const Image = () => (
 
 const Title = () => <h1>The Subtle Art of Not Giving a F*ck</h1>;
 
-const Author = () => <h4>Mark Manson</h4>;
+const Author = () => <h4 style={{color: '#617d98', fontSize:'0.75rem', marginTop:'0.25rem'}}>Mark Manson</h4>;
 
 
 ReactDom.render(<BookList/>, document.getElementById('root'))
