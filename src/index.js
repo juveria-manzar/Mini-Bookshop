71856738eac41c5b9d20004efa773348c7972530
Juveria Manzar <juveriamanzar29@gmail.com>
2020-10-17 08:54:47 +0530

    props

--- src/index.js
+++ src/index.js
@@ -1,43 +1,59 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+//CSS
 import './index.css';
+//setup Vars;
+const firstBook = {
+  title : "The Subtle Art of Not Giving a F*ck",
+  author:"Mark Manson",
+  img : "https://images-eu.ssl-images-amazon.com/images/I/71t4GuxLCuL._AC_UL200_SR200,200_.jpg"
+}
+
+const secondBook = {
+  title: "The Alchemist",
+  author: "Paulo Coelho ",
+  img:
+    "https://images-eu.ssl-images-amazon.com/images/I/71aFt4%2BOTOL._AC_UL200_SR200,200_.jpg",
+};
 
 const BookList = ()=>{
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
-  const title ='The Subtle Art of Not Giving a F*ck'
-  const author = "Mark Manson";
+const Book = (props) => {
   return (
     <article className="book">
-      <Image />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author.toUpperCase()}</h4>
     </article>
   );
 }
 
-const Image = () => (
-  <img
-    src="https://images-eu.ssl-images-amazon.com/images/I/71t4GuxLCuL._AC_UL200_SR200,200_.jpg"
-    alt=""
-  />
-);
+// const Image = () => (
+//   <img
+//     src={firstBook.img}
+//     alt=""
+//   />
+// );
 
-const Title = () => <h1>The Subtle Art of Not Giving a F*ck</h1>;
+// const Title = () => <h1>The Subtle Art of Not Giving a F*ck</h1>;
 
-const Author = () => <h4 style={{color: '#617d98', fontSize:'0.75rem', marginTop:'0.25rem'}}>Mark Manson</h4>;
+// const Author = () => <h4 style={{color: '#617d98', fontSize:'0.75rem', marginTop:'0.25rem'}}>Mark Manson</h4>;
 
 
 ReactDom.render(<BookList/>, document.getElementById('root'))
