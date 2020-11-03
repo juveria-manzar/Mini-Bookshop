8928781c2c44c21fb9af017fb46595bfbf8aa49a
Juveria Manzar <juveriamanzar29@gmail.com>
2020-11-03 08:15:53 +0530

    event-basics

--- src/index.js
+++ src/index.js
@@ -109,7 +109,9 @@ const Book = (props) => {
     console.log(author)
   }
   return (
-    <article className="book">
+    <article className="book" onMouseOver={()=>{
+      console.log(title)
+    }}>
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
